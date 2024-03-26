8c4d55f7e065e33844c48965336e759b02a49efd
y-kakinuma <y-kakinuma@paronym.jp>
2024-03-26 09:19:54 +0900

    install svelte

---
+++ frontend/src/routes/example/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+  // Add your script code here
+</script>
+
+<svelte:head>
+  <title>Example</title>
+</svelte:head>
+
+<section>
+  <h1>example</h1>
+  hoge
+</section>
+
+<style>
+  /* Add your styles here */
+</style>
