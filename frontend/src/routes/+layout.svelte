10b1ea174101d1d74c0691aa471e2ee2e8bd312c
y-kakinuma <y-kakinuma@paronym.jp>
2024-03-31 18:03:36 +0900

    install flowbite-svelte

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script>
+    import '../app.pcss';
 </script>
 
 <div class="app">
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,11 +1,10 @@
 <script>
+    import { Alert } from 'flowbite-svelte';
 </script>
 
-<svelte:head>
-    <title>Home</title>
-</svelte:head>
-
-<section></section>
-
-<style>
-</style>
+<div class="p-8">
+    <Alert>
+        <span class="font-bold">Info alert!</span>
+        Change a few things up and try submitting again.
+    </Alert>
+</div>
