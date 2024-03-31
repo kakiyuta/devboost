0a9e3af1a01ced08a285c04ff35274da85dd54c0
y-kakinuma <y-kakinuma@paronym.jp>
2024-03-31 18:07:16 +0900

    install flowbite-svelte-icons

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,9 +1,11 @@
 <script>
     import { Alert } from 'flowbite-svelte';
+    import { InfoCircleSolid } from 'flowbite-svelte-icons';
 </script>
 
 <div class="p-8">
     <Alert>
+        <InfoCircleSolid slot="icon" class="w-4 h-4" />
         <span class="font-bold">Info alert!</span>
         Change a few things up and try submitting again.
     </Alert>
